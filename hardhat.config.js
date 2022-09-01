b21661c5f2ea4e500f15eca26c330478d5adc280
Fabio Jun <fabiojundev@gmail.com>
2022-09-01 13:06:36 -0300

    Add polygon mumbai network for tests

--- hardhat.config.js
+++ hardhat.config.js
@@ -24,5 +24,9 @@ module.exports = {
       url: process.env.STAGING_ALCHEMY_KEY,
       accounts: [process.env.PRIVATE_KEY],
     },
+    mumbai: {
+      url: process.env.STAGING_MUMBAI_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
   },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -13,31 +13,27 @@ const main = async () => {
         [80, 100, 40], //aerials
         "Pipeline - Hawaii",
         "https://i.imgur.com/GtFuybO.jpg",
-        1000, //boss HP
+        1000, //boss waves
         10 //boss Atack
     );
-
     await gameContract.deployed();
-    console.log("Contrato implantado no endereço:", gameContract.address)
+    console.log("Contrato implantado no endereço:", gameContract.address);
 
     let txn;
-    txn = await gameContract.mintCharacterNFT(0);
-    await txn.wait();
-    console.log("Mintou NFT #1");
-
-    txn = await gameContract.mintCharacterNFT(1);
-    await txn.wait();
-    console.log("Mintou NFT #2");
-
+    // Só temos três personagens.
+    // Uma NFT com personagem no index 2 da nossa array.
     txn = await gameContract.mintCharacterNFT(2);
     await txn.wait();
-    console.log("Mintou NFT #3");
 
-    txn = await gameContract.mintCharacterNFT(1);
+    // Pega o valor da URI da NFT
+    let returnedTokenUri = await gameContract.tokenURI(1);
+    console.log("Token URI:", returnedTokenUri);
+
+    txn = await gameContract.attackBoss();
     await txn.wait();
-    console.log("Minted NFT #4");
-
-    console.log("Fim do deploy e mint!");
+    
+    txn = await gameContract.attackBoss();
+    await txn.wait();
 };
 
 const runMain = async () => {
